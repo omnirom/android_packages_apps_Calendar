5d80ca5bd7eafe218e273aa1bfcf37e61cb9e0b4
maxwen <max.weninger@gmail.com>
2018-08-10 14:16:44 +0200

    Calendar: Pie resurrection
    Change-Id: Ib3ee1a16d3ab216bdc4b4b6afdfe8828397b65d5

--- Android.mk
+++ Android.mk
@@ -5,31 +5,23 @@ include $(CLEAR_VARS)
 chips_dir := ../../../frameworks/opt/chips/res
 color_picker_dir := ../../../frameworks/opt/colorpicker/res
 timezonepicker_dir := ../../../frameworks/opt/timezonepicker/res
-appcompat_dir := ../../../$(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res
 res_dirs := $(chips_dir) $(color_picker_dir) $(timezonepicker_dir) res
 src_dirs := src
-
-LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.calendar.*
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under,$(src_dirs))
 
-# bundled
-#LOCAL_STATIC_JAVA_LIBRARIES += \
-#        android-common \
-#        libchips \
-#        calendar-common
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat
 
-# unbundled
 LOCAL_STATIC_JAVA_LIBRARIES := \
         android-common \
         libchips \
         colorpicker \
         android-opt-timezonepicker \
-        android-support-v4 \
-        calendar-common \
-        android-support-v7-appcompat
+        calendar-common
 
 LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 24
@@ -38,16 +30,14 @@ LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 
 LOCAL_PACKAGE_NAME := Calendar
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_PROGUARD_ENABLED := disabled
+
+LOCAL_USE_AAPT2 := true
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips
 LOCAL_AAPT_FLAGS += --extra-packages com.android.colorpicker
 LOCAL_AAPT_FLAGS += --extra-packages com.android.timezonepicker
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 
 include $(BUILD_PACKAGE)
-
-# Use the following include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
 
